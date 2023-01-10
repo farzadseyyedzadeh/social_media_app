ecf6f932ea9556aa57ca7b22532d98f613c8b1ec
farzad seyyedzadeh <iamseyfarsey@gmail.com>
2023-01-10 10:43:53 +0330

    Demo

---
+++ lib/models/user_model.dart
@@ -0,0 +1,21 @@
+import '/models/post_model.dart';
+
+class User {
+  final String profileImageUrl;
+  final String backgroundImageUrl;
+  final String name;
+  final int following;
+  final int followers;
+  final List<Post> posts;
+  final List<Post> favorites;
+
+  User({
+    required this.profileImageUrl,
+    required this.backgroundImageUrl,
+    required this.name,
+    required this.following,
+    required this.followers,
+    required this.posts,
+    required this.favorites,
+  });
+}
