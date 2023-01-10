268463ee036710e11430f362387e00b46591c127
farzad seyyedzadeh <iamseyfarsey@gmail.com>
2023-01-10 10:46:03 +0330

    data v0.1

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -19,7 +19,14 @@ final _post0 = Post(
 );
 final _post1 = Post(
   imageUrl: 'assets/images/post1.jpg',
-  author: User(),
+  author: User(
+      profileImageUrl: 'assets/images/user0.jpg',
+      backgroundImageUrl: 'assets/images/user.jpg',
+      name: 'farzad',
+      following: 450,
+      followers: 850,
+      posts: posts,
+      favorites: _yourFavorites),
   title: 'Post 1',
   location: 'Location 1',
   likes: 532,
@@ -27,7 +34,14 @@ final _post1 = Post(
 );
 final _post2 = Post(
   imageUrl: 'assets/images/post2.jpg',
-  author: User(),
+  author: User(
+      profileImageUrl: 'assets/images/user0.jpg',
+      backgroundImageUrl: 'assets/images/user.jpg',
+      name: 'farzad',
+      following: 450,
+      followers: 850,
+      posts: posts,
+      favorites: _yourFavorites),
   title: 'Post 2',
   location: 'Location 2',
   likes: 985,
@@ -35,7 +49,14 @@ final _post2 = Post(
 );
 final _post3 = Post(
   imageUrl: 'assets/images/post3.jpg',
-  author: User(),
+  author: User(
+      profileImageUrl: 'assets/images/user0.jpg',
+      backgroundImageUrl: 'assets/images/user.jpg',
+      name: 'farzad',
+      following: 450,
+      followers: 850,
+      posts: posts,
+      favorites: _yourFavorites),
   title: 'Post 3',
   location: 'Location 3',
   likes: 402,
@@ -43,7 +64,14 @@ final _post3 = Post(
 );
 final _post4 = Post(
   imageUrl: 'assets/images/post4.jpg',
-  author: User(),
+  author: User(
+      profileImageUrl: 'assets/images/user0.jpg',
+      backgroundImageUrl: 'assets/images/user.jpg',
+      name: 'farzad',
+      following: 450,
+      followers: 850,
+      posts: posts,
+      favorites: _yourFavorites),
   title: 'Post 4',
   location: 'Location 4',
   likes: 628,
@@ -51,7 +79,14 @@ final _post4 = Post(
 );
 final _post5 = Post(
   imageUrl: 'assets/images/post5.jpg',
-  author: User(),
+  author: User(
+      profileImageUrl: 'assets/images/user0.jpg',
+      backgroundImageUrl: 'assets/images/user.jpg',
+      name: 'farzad',
+      following: 450,
+      followers: 850,
+      posts: posts,
+      favorites: _yourFavorites),
   title: 'Post 5',
   location: 'Location 5',
   likes: 299,
@@ -59,15 +94,7 @@ final _post5 = Post(
 );
 
 final posts = [_post0, _post1, _post2, _post3, _post4, _post5];
-final users = [
-  User(profileImageUrl: 'assets/images/user0.jpg'),
-  User(profileImageUrl: 'assets/images/user1.jpg'),
-  User(profileImageUrl: 'assets/images/user2.jpg'),
-  User(profileImageUrl: 'assets/images/user3.jpg'),
-  User(profileImageUrl: 'assets/images/user4.jpg'),
-  User(profileImageUrl: 'assets/images/user5.jpg'),
-  User(profileImageUrl: 'assets/images/user6.jpg'),
-];
+
 final _yourPosts = [_post1, _post3, _post5];
 final _yourFavorites = [_post0, _post2, _post4];
 
